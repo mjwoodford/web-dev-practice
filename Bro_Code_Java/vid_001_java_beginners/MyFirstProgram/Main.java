da46a8bfaa131dc888a2eafac5ce5495649b59db
mjwoodford <matthewjwoodford@gmail.com>
2025-01-04 15:01:50 -0600

    created vid_001 for Java

---
+++ Bro_Code_Java/vid_001_java_beginners/MyFirstProgram/Main.java
@@ -0,0 +1,17 @@
+package MyFirstProgram;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.print("\nHello World");
+		System.out.println("\nUnforunately, It is raining right now.");
+		System.out.println("\t\"I like the rain, though.\"\n");
+
+		// This is a comment, kind of crazy
+
+		/*
+		 * greetings
+		 
+		   kind of nutty, right?
+		 */
+	}
+}
